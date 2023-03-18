fd88fc33d8d97e4a124d6257c512322cf0b0b00e
Pratik-Nawale <pratiknawale03@gmail.com>
2023-03-18 08:38:30 +0530

    add messaging feature, make mobile responsive

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
+const { Socket } = require("dgram");
 
 const app = express();
 const port = 4500 || process.env.PORT;
 
-
+const users = [{}];
 
 app.use(cors());
 
@@ -18,8 +19,37 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New Connection");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    // console.log(users[socket.id]);
+    console.log(`${user} has joined `);
+
+    // broadcast is used to to send message to everyone except the user
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]} has joined`,
+    });
+
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `Welcome to the chat, ${users[socket.id]} `,
+    });
+  });
+
+  socket.on("message", ({message, id}) => {
+      io.emit("sendMessage", { user: users[id] ,message, id})
+  })
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", {
+      user: "Admin",
+      message: `${users[socket.id]} has left`,
+    });
+    console.log(`user left`);
+  });
 });
 
 server.listen(port, () => {
